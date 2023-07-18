761c9f4a34bc1b7d4a225e5d877e41c21a232289
navis23 <napisszarkasy@gmail.com>
2023-07-18 16:15:21 +0700

    installation of tailwind css

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex w-full min-h-screen text-gray-800">
+        <div class="w-full">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
